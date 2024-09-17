18c88f38508996bc35fd01366f02a4e363e1b999
Ashar <asharmeraj55@gmail.com>
2024-09-17 13:09:00 +0500

    Initial Commit

--- src/app/AllProducts/page.tsx
+++ src/app/AllProducts/page.tsx
@@ -3,19 +3,18 @@ import ProductCard from '../ProductPage/ProductCard'
 import { fetchData } from '../utils/api'
 import { Main } from '../ProductPage/ProductList/ProductType'
 
-const getData = async () => {
-  const prod: Main = await fetchData('/api/products?populate=*')
-  return prod
-}
+// const getData = async () => {
+//   return prod
+// }
 
 
 const page = async () => {
-  const products = await getData()
+  const prod: Main = await fetchData('/api/products?populate=*')
   return (
       <section className='flex justify-center'>
         <main className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  my-[4rem] gap-[3rem]'>
           {
-            products?.data?.map((product: any) => (
+            prod.data.map((product: any) => (
               <ProductCard key={product.id} data={product} />
             ))
           }
